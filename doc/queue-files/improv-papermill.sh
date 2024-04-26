23caccefa122bd58a0fec15d92d120c76e17bc16
Logan Ward <ward.logan.t@gmail.com>
2024-04-26 11:28:10 -0500

    Add example queue file for running notebook

---
+++ doc/queue-files/improv-papermill.sh
@@ -0,0 +1,25 @@
+#PBS -A Athena
+#PBS -l select=1:ncpus=128:mpiprocs=128
+#PBS -l walltime=0:30:00
+
+
+# Load the conda environment
+source activate /lcrc/project/Athena/cascade/env
+which python
+
+# Load environment
+module load gcc openmpi
+module load cp2k
+module list
+
+nnodes=`wc -l $PBS_NODEFILE`
+ranks_per_node=16
+total_ranks=$(($ranks_per_node * $nnodes))
+threads_per_rank=$((128 / ranks))
+
+export OMP_NUM_THREADS=$threads_per_rank
+export ASE_CP2K_COMMAND="mpirun -N $total_ranks -n $ranks_per_node --cpus-per-proc $threads_per_rank /lcrc/project/Athena/cp2k/exe/local/cp2k_shell.psmp"
+
+# Run the stuff
+cd $PBS_O_WORKDIR
+papermill 1_relax-initial-geometry.ipynb /dev/null
