7fbdae60e3b2f7d1a8d189a6a485d10009f5d214
Logan Ward <ward.logan.t@gmail.com>
2024-04-26 12:43:59 -0500

    Allow CP2K to span >1 node

--- doc/queue-files/improv-papermill.sh
+++ doc/queue-files/improv-papermill.sh
@@ -1,5 +1,5 @@
 #PBS -A Athena
-#PBS -l select=1:ncpus=128:mpiprocs=128
+#PBS -l select=2:ncpus=128:mpiprocs=128
 #PBS -l walltime=0:30:00
 
 
@@ -12,10 +12,12 @@ module load gcc openmpi
 module load cp2k
 module list
 
-nnodes=`wc -l $PBS_NODEFILE`
+nnodes=`cat $PBS_NODEFILE | sort | uniq | wc -l`
 ranks_per_node=16
 total_ranks=$(($ranks_per_node * $nnodes))
-threads_per_rank=$((128 / ranks))
+threads_per_rank=$((128 / ranks_per_node))
+
+echo Running $total_ranks ranks across $nnodes nodes with $threads_per_rank threads per rank
 
 export OMP_NUM_THREADS=$threads_per_rank
 export ASE_CP2K_COMMAND="mpirun -N $total_ranks -n $ranks_per_node --cpus-per-proc $threads_per_rank /lcrc/project/Athena/cp2k/exe/local/cp2k_shell.psmp"
