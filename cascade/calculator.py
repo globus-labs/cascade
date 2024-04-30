3c3904742007c3024ebb1234f04a08585a55dc91
Logan Ward <ward.logan.t@gmail.com>
2024-04-30 15:22:14 -0400

    Do not hard-code configs within Python function

--- cascade/calculator.py
+++ cascade/calculator.py
@@ -4,6 +4,7 @@ from string import Template
 
 from ase.calculators.cp2k import CP2K
 from ase import units
+import yaml
 
 _file_dir = Path(__file__).parent / 'files'
 
@@ -16,13 +17,6 @@ def make_calculator(
 ) -> CP2K:
     """Make a calculator ready to run with different configurations
 
-    Supported methods:
-        - `pm6`: A force-matched PM6 shown by `Welborn et al <https://onlinelibrary.wiley.com/doi/10.1002/jcc.23887>`_
-           to agree better for properties of liquid water than the original formulation
-        - `blyp`: The BLYP GGA potential with D3 vdW corrections, suggested by `Lin et al. <https://pubs.acs.org/doi/10.1021/ct3001848>`_
-            to give the best properties of liquid water
-        - `b97m`: The `B97M-rV <http://xlink.rsc.org/?DOI=C6SC04711D>`_ metaGGA functional
-
     Args:
         method: Which method to run
         multiplicity: Multiplicity of the system
@@ -32,39 +26,30 @@ def make_calculator(
         Calculator configured for target method
     """
 
-    # Get the input file and cutoff energy
+    # Load the presets file
+    with (_file_dir / 'presets.yml').open() as fp:
+        presets = yaml.safe_load(fp)
+    if method not in presets:
+        raise ValueError(f'"{method}" not in presets file')
+    kwargs = presets[method]
+    if kwargs.get('cutoff') is not None:
+        kwargs['cutoff'] *= units.Ry
+    kwargs.pop('description')
+
+    # Get the input file and replace any templated arguments
     input_file = _file_dir / f'cp2k-{method}-template.inp'
-    cutoff = {
-        'b97m': 800 * units.Ry,
-        'blyp': 500 * units.Ry
-    }.get(method, None)
-    max_scf = {'b97m': 32}.get(method, 128)
-
-    # Get the basis set and potential type
-    basis_set = {
-        'blyp': 'DZVP-MOLOPT-SR-GTH',
-        'b97m': 'DZVP-MOLOPT-SR-GTH',
-    }.get(method)
-    potential = {
-        'blyp': 'GTH-PBE',
-        'b97m': 'GTH-BLYP'
-    }.get(method)
-
-    inp = Template(input_file.read_text()).substitute(mult=multiplicity)  # No changes as of it
+    inp = Template(input_file.read_text()).substitute(mult=multiplicity)
 
     cp2k_opts = dict(
         xc=None,
         inp=inp,
-        basis_set=basis_set,
-        pseudo_potential=potential,
         poisson_solver=None,
+        **kwargs
     )
     if command is not None:
         cp2k_opts['command'] = command
     return CP2K(directory=directory,
                 stress_tensor=True,
-                max_scf=max_scf,
-                cutoff=cutoff,
                 potential_file=None,
                 set_pos_file=True,
                 **cp2k_opts)
