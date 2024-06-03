122ccff4db066a3fd0251bc86348df53b49a0f6a
Michael Tynes <mtynes@uchicago.edu>
2024-06-03 10:54:29 -0500

    Ensemble (#9)
    * require parsl
    
    * first pass local parsl structure generation
    
    * switch from threads to local_htex
    
    * fix docs
    
    * rm init structure notebook (replaced with parsl script)
    
    * export to scripts
    
    * wip: parslify relaxation
    
    * Introduce installation instructions
    
    * Combine documentation directories
    
    * Add documentation for running the notebooks
    
    * Flake8 fix
    
    * Actually flake8 fix
    
    * require parsl
    
    * first pass local parsl structure generation
    
    * switch from threads to local_htex
    
    * fix docs
    
    * rm init structure notebook (replaced with parsl script)
    
    * export to scripts
    
    * wip: parslify relaxation
    
    * wip: parsl almost working, cp2k complaining
    
    The current parsl config runs until cp2k complains that it doesnt
    have access to enough slots to run what we request.
    
    I think this is related to the MPI + parsl issues that Logan
    was describing
    
    * achieve new and different errors
    
    now it cannot get resources for the blocks it asks for
    and for some reason it asks for 12 blocks instead of 1
    and I have no idea why
    
    * get relaxation to launch a node; still not sure if it works
    
    * launches but chokes
    
    * run with mpich
    
    * walkback runtime
    
    * rm relax ipynb
    
    * remove unused code
    
    * parslize equilibration
    
    * rm equil ipynb
    
    * wip: ensemble training
    
    * train 4 replicas
    
    * WIP: notebooks looking at different UQ approaches
    
    we try out
    * energy variance
    * min/max force variance
    * variance over the ensemble run
    
    * add discussion/next steps for force UQ
    
    ---------
    
    Co-authored-by: Logan Ward <ward.logan.t@gmail.com>

---
+++ 1_mace/1_train-mace-ensemble.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for seed in {0..3}; do
+    echo "training replica $seed"
+    time python 1_train-mace-replica.py \
+    --seed $seed \
+    --train-files ../0_setup/md/packmol-CH4-in-H2O\=32-seed={0..2}*/md.traj \
+    |tee mlrun.log 2>&1
+done
