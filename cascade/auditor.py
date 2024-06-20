63dd0c25100b3219170793401ad0aab20adc071f
miketynes <mtynes@uchicago.edu>
2024-06-20 13:32:14 -0500

    WIP threshold audit

--- cascade/auditor.py
+++ cascade/auditor.py
@@ -1,3 +1,5 @@
+import numpy as np
+from scipy.stats import zscore
 from ase import Atoms
 
 class BaseAuditor: 
@@ -9,10 +11,18 @@ class BaseAuditor:
 class ForceThresholdAuditor(BaseAuditor): 
     """Determines the likelihood all calculations are below the threshold"""
     correction_factor : float
+    threshold         : float
 
-    def audit(self, atoms: list[Atoms], n_audits: int) -> tuple[float, list[Atoms]]:
+    def audit(self, 
+              atoms: list[Atoms], 
+              n_audits: int, 
+              uq_key: str = 'force_std_max') -> tuple[float, list[Atoms]]:
+        """"""
         # compute z-scores (force ens deviation * correction_factor) across whole trajectory
         # multiply them as if they are independent. this gives you the probability 
+        traj_uq = np.asarray([a.info[uq_key] for a in atoms])
+        z = zscore(traj_uq*self.correction_factor)
+
         pass
 
     # todo: think about this. Need a way to update the correction factor
