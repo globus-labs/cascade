10725602188e3361296639f9026bf951dc2f84e1
Logan Ward <ward.logan.t@gmail.com>
2024-07-05 14:18:25 -0400

    Flake8 fixes

--- cascade/learning/torchani/__init__.py
+++ cascade/learning/torchani/__init__.py
@@ -9,9 +9,7 @@ from torchani import AEVComputer, ANIModel
 from torchani.aev import SpeciesAEV
 from torchani.data import collate_fn
 from ignite.engine import Engine, Events
-from ignite.handlers.early_stopping import EarlyStopping
 import torch
-
 
 from cascade.learning.base import BaseLearnableForcefield, State
 from cascade.learning.utils import estimate_atomic_energies
@@ -166,22 +164,6 @@ class TorchANI(BaseLearnableForcefield[ANIModelContents]):
               force_weight: float = 0.9,
               reset_weights: bool = False,
               **kwargs) -> tuple[bytes, pd.DataFrame]:
-        """Training method for a torchani model
-
-
-        Args:
-            model_msg: serialized aev computer, torchani model, and species 
-            train_data: atoms to train to
-            valid_data: atoms to validate against
-            num_epochs: number of training epochs
-            device: compute device for training (e.g. "cpu", "cuda:0")
-            batch_size: number of training points in a batch
-            learning_rate: traianing learning weight
-            huber_deltas: energy, force deltas in huber loss
-            force_weight: weight on force in loss 
-            reset_weights: whether to reinitialize the model weights
-        """
-
         # Unpack the model and move to device
         if isinstance(model_msg, bytes):
             model_msg = self.get_model(model_msg)
@@ -241,17 +223,16 @@ class TorchANI(BaseLearnableForcefield[ANIModelContents]):
             loss.backward()
             opt.step()
             return loss.item()
-        
 
-        def evaluate_model(loader: DataLoader, 
+        def evaluate_model(loader: DataLoader,
                            accumulator: list[dict[str, float]]) -> None:
             """Evaluate the model against all data in loader, store in global list
-            
-            Args: 
+
+            Args:
                 loader: a pytorch data loader
-                acccumulator: a list in which to append results  
+                accumulator: a list in which to append results
             """
-            e_qm, e_ml, f_qm, f_ml = [], [], [],[]
+            e_qm, e_ml, f_qm, f_ml = [], [], [], []
             for batch in loader:
                 # Get the true results
                 batch_e = batch['energies'].cpu().numpy().squeeze()
@@ -268,35 +249,35 @@ class TorchANI(BaseLearnableForcefield[ANIModelContents]):
                 f_ml.extend(batch_f_pred.detach().cpu().numpy())
 
             # convert batch results into flat np arrays
-            e_qm, e_ml, f_qm, f_ml = map(lambda l: np.asarray(l).ravel(), 
-                                       (e_qm, e_ml, f_qm, f_ml))
-            
+            e_qm, e_ml, f_qm, f_ml = map(lambda a: np.asarray(a).ravel(),
+                                         (e_qm, e_ml, f_qm, f_ml))
+
             # compute and store metrics
             result = {}
-            result['e_rmse'] = np.sqrt((e_qm - e_ml)**2).mean()
+            result['e_rmse'] = np.sqrt((e_qm - e_ml) ** 2).mean()
             result['e_mae'] = np.abs(e_qm - e_ml).mean()
-            result['f_rmse'] = np.sqrt((f_qm - f_ml)**2).mean()
+            result['f_rmse'] = np.sqrt((f_qm - f_ml) ** 2).mean()
             result['f_mae'] = np.abs(f_qm - f_ml).mean()
             accumulator.append(result)
-            return 
-     
+            return
+
         # instantiate the trainer
         trainer = Engine(train_step)
 
-        # Set up model evaluators 
+        # Set up model evaluators
         # TODO (miketynes): make this more idiomatic pytorch ignite
         # TODO (miketynes): add early stopping (probably depends on the above)
-        perf_train : list[dict[str, float]] = [] 
-        perf_val   : list[dict[str, float]] = []
+        perf_train: list[dict[str, float]] = []
+        perf_val: list[dict[str, float]] = []
         evaluator_train = partial(evaluate_model,
-                                  loader=train_loader, 
+                                  loader=train_loader,
                                   accumulator=perf_train)
-        evaluator_val   = partial(evaluate_model,
-                                  loader=valid_loader, 
-                                  accumulator=perf_val)
+        evaluator_val = partial(evaluate_model,
+                                loader=valid_loader,
+                                accumulator=perf_val)
         trainer.add_event_handler(Events.EPOCH_COMPLETED, evaluator_train)
         trainer.add_event_handler(Events.EPOCH_COMPLETED, evaluator_val)
-        
+
         # Run the training
         trainer.run(train_loader, max_epochs=num_epochs)
 
@@ -307,5 +288,5 @@ class TorchANI(BaseLearnableForcefield[ANIModelContents]):
         perf = pd.concat([perf_train, perf_val]).reset_index(names='iteration')
 
         # serialize model
-        model_msg = self.serialize_model((aev_computer, model, atomic_energies)) 
-        return model_msg, perf 
+        model_msg = self.serialize_model((aev_computer, model, atomic_energies))
+        return model_msg, perf
