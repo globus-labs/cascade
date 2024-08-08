0da247a9dddba36668df6e40717d74c96c342a70
Logan Ward <ward.logan.t@gmail.com>
2024-08-08 14:23:43 -0400

    Implement calculator conversion

--- cascade/learning/chgnet.py
+++ cascade/learning/chgnet.py
@@ -1,11 +1,12 @@
 """Training based on the `"ChargeNet" neural network <https://doi.org/10.1038/s42256-023-00716-3>`_"""
-import ase
 import numpy as np
-
-from chgnet.model import CHGNet
+import ase
+from ase import units
+from ase.calculators.calculator import Calculator
+from chgnet.model import CHGNet, CHGNetCalculator
 from pymatgen.io.ase import AseAtomsAdaptor
 
-from .base import BaseLearnableForcefield
+from .base import BaseLearnableForcefield, State
 
 
 class CHGNetInterface(BaseLearnableForcefield[CHGNet]):
@@ -16,9 +17,7 @@ class CHGNetInterface(BaseLearnableForcefield[CHGNet]):
                  atoms: list[ase.Atoms],
                  batch_size: int = 64,
                  device: str = 'cpu') -> (np.ndarray, list[np.ndarray], np.ndarray):
-        model = model_msg
-        if isinstance(model_msg, bytes):
-            model = self.get_model(model_msg)
+        model = self.get_model(model_msg)
 
         # Convert structures to Pymatgen
         structures = [AseAtomsAdaptor.get_structure(a) for a in atoms]
@@ -30,6 +29,13 @@ class CHGNetInterface(BaseLearnableForcefield[CHGNet]):
 
         # Transpose into Numpy arrayes
         energies = np.array([r['e'] for r in preds])
+        if model.is_intensive:
+            atom_counts = np.array([len(a) for a in atoms])
+            energies *= atom_counts
         forces = [r['f'][:len(a), :] for a, r in zip(atoms, preds)]
-        stress = np.array([r['s'] for r in preds])
+        stress = np.array([r['s'] for r in preds]) * units.GPa
         return energies, forces, stress
+
+    def make_calculator(self, model_msg: bytes | State, device: str) -> Calculator:
+        model = self.get_model(model_msg)
+        return CHGNetCalculator(model=model, use_device=device)
--- tests/learning/test_chgnet.py
+++ tests/learning/test_chgnet.py
@@ -1,5 +1,6 @@
 from chgnet.model import CHGNet
 from pytest import fixture
+import numpy as np
 
 from cascade.learning.chgnet import CHGNetInterface
 
@@ -17,3 +18,11 @@ def test_inference(chgnet, example_data):
     for atoms, f in zip(example_data, forces):
         assert f.shape == (len(atoms), 3)
     assert stresses.shape == (2, 3, 3)
+
+    # Test the calculator interface
+    calc = cg.make_calculator(chgnet, 'cpu')
+    atoms = example_data[0]
+    atoms.calc = calc
+    assert np.isclose(atoms.get_potential_energy(), energy[0]).all()
+    assert np.isclose(atoms.get_forces(), forces[0]).all()
+    assert np.isclose(atoms.get_stress(voigt=False), stresses[0]).all()
