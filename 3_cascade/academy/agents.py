b9d29adb2a229edf0ac6a2fa54d441a39de61322
miketynes <mtynes@uchicago.edu>
2025-10-22 12:18:02 -0500

    WIP agentic implementation

--- 3_cascade/academy/agents.py
+++ 3_cascade/academy/agents.py
@@ -1,148 +1,294 @@
+"""WIP agentic implementation of cascasde with academy
+
+Everything here is a stub
+
+In addition to de-stubbing, we have the following todos:
+* thread/process safety for queues, model updates
+* rewrite for concurrent, high performance DB backend
+* data model improvements
+* add parallelism within agents with parsl
+* make configuration of agents flexible, updatable (i.e., control parameters)
+* logging
+"""
+
 import asyncio
 from typing import Callable
+from collections import defaultdict
+from asyncio import Queue
 
 import numpy as np
 from numpy.random import sample
 from ase import Atoms
 from ase.db import connect
 from academy.handle import Handle
-from academy.agent import Agent, action
+from academy.agent import Agent, action, loop
 from ase.calculators.calculator import Calculator
 from ase.optimize.optimize import Dynamics
+from mace.calculators import mace_mp
 
 from cascade.learning.base import BaseLearnableForcefield
 from cascade.utils import canonicalize
+from model import AuditStatus
 
 from base_agents import (
-    Auditor, TrainingSampler, TrainingLabeler, ModelTrainer
+    Auditor, TrainingSampler, TrainingLabeler, ModelTrainer, Database
 )
 
-# class CascadseCoordinator(Agent):
 
-#     def __init__(
-#         self,
-#         dynamics_engine: Handle[DynamicsEngine],
-#         auditor: Handle[Auditor],
-#         training_sampler: Handle[TrainingSampler],
-#     ):
-#         pass
+class DummyDatabase(Database):
+    """Not a real database, just something that will work for now"""
 
+    def __init__(
+        self, 
+        trajectories: list[Trajectories],
+        retrain_len: int,
+        trainer: Handle[ModelTrainer],
+        dynamics_engine: Handle[DynamicsEngine]
+    ):
 
-class Writer(Agent):
-    """some design questions:
-    
-    * should this thing maintain a connection for its lifetime?
-    * how parallel (if at all) should this be?
-    * should the chunk index just be in a kwargs? or some dataclass?
-    * should it figure out the pass index?
-    """
+        self.trajectories = trajectories
+        self.training_frames = []
+        self.sampled_trajectories = set()
+        self.retrain_len = retrain_len
+        self.trainer = trainer
+        self.dynamics_engine = dynamics_engine
 
-    def __init__(self, db_path: str):
-        self.db_path = db_path
+    @property
+    def all_done(self):
+        return all(t.done for t in self.trajectories)
 
     @action
-    async def write(
-        self,
-        atoms: list[Atoms],
-        db_path: str = None,
-        **kwargs
-    ):
-        """Write chunk of trajectory to a database
-        atoms: trajectory to write
-        db_path: overrides that passed to initializer
-        kwargs: passed to write for each frame
-        """
+    async def write_chunk(self, chunk: TrajectoryChunk):
+        id = chunk.traj_id
+        self.trajectories[traj_id].add_chunk(chunk)
 
-        # handle defaults
-        db_path = db_path if db_path is not None else self.db_path
-        print('connecting to db')
-        with connect(db_path) as db:
-            for a in atoms:
-                db.write(a, **kwargs)
+    @action
+    async def write_training_frame(self, frame: Atoms):
+        # check here if we have exceeded the retrain length, then set event
+        self.training_frames.append(frame)
+    
+    @action
+    async def mark_sampled(self, traj_id: int):
+        self.sampled_trajectories.add(traj_id)
 
+    @loop
+    async def periodic_retrain(self, shutdown: asyncio.Event):
+        # run this thing on event (see the sc25 example)
+        """Trigger a retrain event and add the right atoms back to the dynamics queue"""
+        while not shutdown.is_set():
+            if len(training_frames) <= self.retrain_len:
+                continue # sleep?
+            
+            # retrain model and update weights in dynamics engine
+            weights = await self.trainer.retrain()
+            await self.dynamics_engine.receive_weights(weights)
+
+            # put the sampled trajectories back in the dynamics queue
+            for traj_id in self.sampled_trajectories:
+                atoms = self.trajecotires[traj_id].chunks[-1].atoms[-1]
+                await self.dymamics_engine.submit(atoms)
+
+            # clear training data
+            self.training_frames = []
+            self.sampled_frames = set()
+                
 
 class DynamicsEngine(Agent):
 
     def __init__(
-        self
-    ):
-        pass
-
-    @action
-    async def advance_dynamics(
         self,
-        atoms: Atoms,
+        auditor: Handle["Auditor"],
         learner: BaseLearnableForcefield,
-        model_msg: bytes,
-        steps: int,
-        calc_factory: Callable[[], Calculator],
+        weights: bytes,
         dyn_cls: type[Dynamics],
         dyn_kws: dict[str, object],
-        run_kws: dict[str, object] = None
-    ) -> list[Atoms]:
+        run_kws: dict[str, object],
+        device: str = 'cpu'
+    ):
 
-        if run_kws is None:
-            run_kws = {}
+        self.learner = learner
+        self.weights = weights
+        self.dyn_cls = dyn_cls
+        self.device = device
+        self.dyn_kws = dyn_kws
+        self.run_kws = run_kws
+
+        self.model_version = 0  #todo: mt.2025.10.20 probably this should be persisted somewhere else
+
+        self.queue = Queue()
+
+
+    @action
+    async def receive_weights(self, weights:bytes) -> None:
+        self.weights = weights
+        self.model_version += 1
+    
+    @action
+    async def submit(chunk: TrajectoryChunk):
+        self.queue.put(chunk)
+
+    @loop
+    async def advance_dynamics(
+        self,
+        shutdown: asyncio.Event
+    ) -> None:
+    """Advance dynamics while there are trajectoires to advance
+
+
+    questions:
+        * should we have an update model method that locks?
+    """
+
+        while not shutdown.is_set():
+
+            try:
+                old_chunk = self.queue.get()
+            except Queue.Empty:
+                continue
+
+            atoms = old_chunk.atoms[-1]
+ 
+            print('making learner')
+            calc = learner.make_calculator(self.weights, device=self.device)
+            print('done.')
+            atoms.calc = calc
+
+
+            chunk = TrajectoryChunk(
+                atoms=[],
+                model_version = self.model_version,
+                traj_id=old_chunk.traj_id,
+                chunk_id=old_chunk.chunk_id+1
+            )
+
+            # set up dynamics
+            dyn_kws = self.dyn_kws or {}
+            run_kws = self.run_kws or {}
+            dyn = self.dyn_cls(
+                atoms,
+                **dyn_kws
+            )
+
+            # set up writer
+            def write_to_list():
+                # needs to be 64 bit for db read
+                f = atoms.calc.results['forces']
+                atoms.calc.results['forces'] = f.astype(np.float64)
+                chunk.atoms.append(canonicalize(atoms))
+            dyn.attach(write_to_list)
             
-        print('making learner')
-        calc = learner.make_calculator(model_msg, device='cpu')
-        print('done.')
-        atoms.calc = calc
+            # run dynamics
+            print('running dynamics')
+            dyn.run(steps, **run_kws)
+            print('done.')
 
-        traj = []
-
-        # set up writer
-        def write_to_list():
-            # needs to be 64 bit for db read
-            f = atoms.calc.results['forces']
-            atoms.calc.results['forces'] = f.astype(np.float64)
-            traj.append(canonicalize(atoms))
-
-        # set up dynamics
-        dyn = dyn_cls(
-            atoms,
-            **dyn_kws
-        )
-        dyn.attach(write_to_list)
-        # run dynamics
-        print('running dynamics')
-        dyn.run(steps, **run_kws)
-        print('done.')
-        return traj
+            # submit to auditor
+            await auditor.submit(chunk)
 
 
 class DummyAuditor(Auditor):
+    
+    def __init__(
+            self,
+            accept_rate: float = 1.,
+            sampler: Handle["TrainingSampler"],
+            dynamics_engine: Handle["DynamicsEngine"],
+            database: Handle["Database"]
+        ):
 
-    accept_rate = 0.75
+        self.accept_rate = accept_rate
+        self.sampler = sampler
+        self.dynamics_engine = dynamics_engine
+        self.queue = Queue()
+
 
     @action
-    async def audit_chunk(
-        self,
-        traj: list[Atoms]
-    ) -> tuple[bool, float, Atoms]:
-        """a stub of a real audit"""
-        good = np.random.random() < self.accept_rate
-        score = float(good)
-        atoms = traj[-1] if good else traj[0]
-        return good, score, atoms
+    def submit(self, TrajectoryChunk):
+        self.queue.put(TrajectoryChunk)
 
+    @loop
+    async def audit(
+        self,
+        shutdown: asyncio.Event
+    ) -> None:
+        """a stub of a real audit"""
+        
+        while not shutdown.is_set():
+            try:
+                chunk = self.queue.get()
+            except Queue.Empty:
+                continue
+            
+            good = np.random.random() < self.accept_rate
+            score = float(good)
+            if good:
+                chunk.audit_status = AuditStatus.PASSED
+                # todo: check if done
+                await self.dynamics_engine.submit(chunk)
+            else:
+                chunk.audit_status = AuditStatus.FAILED
+                await self.database.mark_sampled(chunk.traj_id)
+                await self.training_sampler.submit(chunk)    
 
 class DummySampler(TrainingSampler):
 
-    @action
-    async def sample_frames(
+    def __init__(
         self,
-        atoms: list[Atoms],
-        n_frames: int
-    ) -> list[Atoms]:
-        return sample(atoms, n_frames)
+        n_frames: int,
+        labeler: Handle[TrainingLabeler]
+        rng: np.random.Generator = None,
+    ):
+        self.rng = rng if rng else np.random.default_rng()
+        self.queue = Queue()
+        self.labeler = labeler
+        self.n_frames = n_frames
+
+    @loop
+    async def sample(
+        self,
+        shudown: asyncio.Event
+    ) -> None:
+        while not shutdown.is_set():
+            
+            try:
+                chunk = self.queue.get()
+            except Queue.Empty:
+                continue
+            
+            atoms = chunk.atoms
+            frames = self.rng.choice(atoms, self.n_frames, replace=False)
+            for frame in frames:
+                await labeler.submit_frame(frame)
+
+class DummyLabeler(TrainingLabeler):
+
+    def __init__(
+        self,
+        databse: Handle[Database]
+    ):
+        self.database = database
+        self.queue = Queue()
+
+    @action
+    async def submit_frame(self, frame: Atoms) -> None:
+
+        self.queue.put(frame)
+
+    @loop
+    async def label_data(self, shutdown: asyncio.Event) -> None:
+        try:
+            frame = self.queue.get()
+        except Queue.Empty:
+            continue
+        
+        await database.write_training_frame(frame)
 
 
 class DummyTrainer(ModelTrainer):
 
     @action
     async def train_model(
-        self, 
+        self,
         learner: BaseLearnableForcefield
     ) -> bytes:
         calc = mace_mp('small', device='cpu', default_dtype="float32")
--- 3_cascade/academy/run_cascade_academy.py
+++ 3_cascade/academy/run_cascade_academy.py
@@ -1,162 +1,104 @@
-import argparse
-from pathlib import Path
-import logging
-import sys
-import os
-from queue import Queue, Empty
-from functools import partial, update_wrapper
-from dataclasses import dataclass
-from random import sample
-from typing import Callable
-from threading import Event
-
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
-from academy.agent import Agent, action
 from academy.exchange import LocalExchangeFactory
-from academy.logging import init_logging
 from academy.manager import Manager
-from academy.handle import Handle
+from concurrent.futures import ThreadPoolExecutor
 
-import ase
-from ase import Atoms
+from agents import (
+    DummyDatabase,
+    DynamicsEngine,
+    DummyAuditor,
+    DummySampler,
+    DummyLabeler,
+    DummyTrainer
+)
+
+from model import Trajectory
+
 from ase.io import read
-from ase.optimize.optimize import Dynamics
-from ase.md.verlet import VelocityVerlet
-from ase import units
-from ase.db import connect
-from ase.calculators.calculator import Calculator
-import numpy as np
-from mace.calculators import mace_mp
 
-from cascade.learning.base import BaseLearnableForcefield
-from cascade.learning.mace import MACEInterface
-from cascade.utils import canonicalize
 
+# args
+init_strc = []
+target_length = 100
+retrain_len = 10
+n_sample_frames = 1
+accept_rate = 1.
+learner = None
+init_weights = None
+dyn_cls = None
+dyn_kws = {}
+run_kws = {}
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        prog='Cascade',
-        description="""Launch a cascade job. Trajectories will be advanced under an ML surrogate 
-        in chunks of size <advance-steps> until they are all of length <total-steps>. 
-        When chunks from <start-train-frac> of the trajectories are marked as untrusted, the model will be updated.
-        """
-    )
-    parser.add_argument('--run-name', type=str, required=True, help='Name that appears in the run directory')
-    parser.add_argument('--advance-steps', type=int, required=True, help='How many steps to advance a trajectory at a time')
-    parser.add_argument('--total-steps', type=int, required=True, help='How long a trajectory should be')
-    parser.add_argument(
-        '--init-strc',
-        type=str,
-        nargs='+',
-        required=True,
-        help='Path(s) to starting structures for molecular dynamics. One trajectory will be run for each starting structure'
-    )
-    parser.add_argument('--start-train-frac', type=float, default=1., help='What fraction of trajectories have to have untrusted segments before training begins')
-    parser.add_argument('--num-workers', type=int, default=8, help='How many workers')
-    #parser.add_argument('--model-file', type=str)
-
-    args = parser.parse_args()
-
-    # Prepare the output directory
-    run_name = args.run_name
-    run_dir = Path('runs') / f'run-{run_name}'
-    is_restart = run_dir.exists()
-    run_dir.mkdir(parents=True, exist_ok=True)
-
-    # set up ase db
-    db_path = run_dir / 'database.db'
-    if os.path.exists(db_path):
-        os.remove(db_path)
-    # (it will be created on first access)
-
-    # Set up Queues and TaskServer
-    queues = PipeQueues(
-        topics=[
-            'dynamics',
-            'audit',
-            'frame_selection',
-            'label',
-            'train',
-        ],
-        keep_inputs=False
-    )
-
-    # read in initial structures
+    
+    # intial conditions, trajectories
     initial_frames = []
-    for s in args.init_strc:
+    trajectories = []
+    for i, s in enumerate(init_strc):
         initial_frames.append(read(s, '-1'))
+        trajectories.append(
+            Trajectory(
+                chunks=[],
+                target_length=target_length,
+                traj_id=i,
+            )
+        )
 
-    # set up learner
-    # todo: make this configurable via CLI
-    learner = MACEInterface()
-    calc = mace_mp('small', device='cpu', default_dtype="float32")
-    model = calc.models[0]
-    model_msg = learner.serialize_model(model)
+    # initialize manager, exhcange
+    async with await Manager.from_exchange_factory(
+        factory=LocalExchangeFactory(),
+        executors=ThreadPoolExecutor(),
+    ) as manager:
 
-    # Set up Thinker
-    thinker = Thinker(
-        n_workers=args.num_workers,
-        initial_frames=initial_frames,
-        queues=queues,
-        run_dir=run_dir,
-        db_path=db_path,
-        advance_steps=args.advance_steps,
-        total_steps=args.total_steps,
-        sample_frames=1,
-        start_train_frac=args.start_train_frac,
-        learner=learner,
-        model_msg=model_msg
-    )
-    # Make a logger
-    logger = logging.getLogger('main')
-    handlers = [logging.StreamHandler(sys.stdout), logging.FileHandler(filename=run_dir / 'run.log')]
-    for handler in handlers:
-        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        for my_logger in [logger, logging.getLogger('cascade'), thinker.logger]:
-            my_logger.addHandler(handler)
-            my_logger.setLevel(logging.INFO)
-    logger.info(f'Started. Running in {run_dir}. {"Restarting from previous run" if is_restart else ""}')
+        # register all agents with manager
+        db_reg = manager.register_agent(DummyDatabase)
+        trainer_reg = manager.register_agent(DummyTrainer)
+        labeler_reg = manager.register_agent(DummyLabeler)
+        sampler_reg = manager.register_agent(DummySampler)
+        auditor_reg = manager.register_agent(DummyAuditor)
+        dynamics_reg = manager.register_agent(DynamicsEngine)
 
-    # set up auditor
-    auditor = DummyAuditor()
+        # get handles to all agents
+        db_handle = manager.get_handle(db_reg)
+        trainer_handle = manager.get_handle(trainer_reg)
+        labeler_handle = manager.get_handle(labeler_reg)
+        sampler_handle = manager.get_handle(sampler_reg)
+        auditor_handle = manager.get_handle(auditor_reg)
+        dynamics_handle = manager.get_handle(dynamics_reg)
 
-    # set up dynamics
-    # todo: make this configurable via CLI
-    dyn_class = VelocityVerlet
-    dyn_kwargs = {'timestep': 1 * units.fs}
-    run_kwargs = {}
+        # launch all agents
+        manager.launch(
+            db_handle,
+            trajectories,
+            retrain_len,
+            trainer_handle,
+            dynamics_handle
+        )
 
-    # wrap run function
-    advance_dynamics_partial = partial(
-        advance_dynamics,
-        learner=learner,
-        # write_freq=1,  # todo: make an arg
-        dyn_class=dyn_class,
-        dyn_kwargs=dyn_kwargs,
-        run_kwargs=run_kwargs,
-    )
-    update_wrapper(advance_dynamics_partial, advance_dynamics)
+        manager.launch(
+            trainer_handle,
+            learner
+        )
 
-    # set up reference calc
-    calc_factory = partial(mace_mp, 'medium', device='cpu', default_type='float32')
-    label_partial = partial(label_training_frame, calc_factory=calc_factory)
-    update_wrapper(label_partial, label_training_frame)
-    # Start the workflow
-    task_server = LocalTaskServer(
-        queues=queues,
-        num_workers=args.num_workers,
-        methods=[
-            advance_dynamics_partial,
-            auditor.audit,
-            select_training_frames,
-            label_partial
-        ]
-    )
-    try:
-        task_server.start()
-        thinker.run()
-    finally:
-        queues.send_kill_signal()
-        task_server.join()
-    logger.info('Done!')
+        manager.launch(
+            sampler_handle,
+            n_sample_frames,
+            sampler_handle
+        )
+
+        manager.launch(
+            auditor_handle,
+            accept_rate,
+            sampler_handle,
+            dynamics_handle,
+            db_handle
+        )
+
+        manager.launch(
+            dynamics_handle,
+            auditor_handle,
+            learner,
+            init_weights,
+            dyn_cls,
+            dyn_kws,
+            run_kws
+        )
