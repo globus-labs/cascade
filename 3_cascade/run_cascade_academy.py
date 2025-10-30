2fdc763de2124118e7588c4ba516389713a5e1c7
miketynes <mtynes@uchicago.edu>
2025-10-30 15:14:01 -0500

    fix path

--- 3_cascade/run_cascade_academy.py
+++ 3_cascade/run_cascade_academy.py
@@ -27,7 +27,7 @@ async def main():
     init_logging(logging.DEBUG)
 
     # args
-    init_strc = ['../../0_setup/final-geometries/packmol-CH4-in-H2O=32-seed=1-mace-medium.vasp'] * 2
+    init_strc = ['../0_setup/final-geometries/packmol-CH4-in-H2O=32-seed=1-mace-medium.vasp'] * 2
 
     target_length = 10
     retrain_len = 100000
