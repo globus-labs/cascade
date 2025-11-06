aa6c31e3a1e62bc31c91d9d0d5d5aaa4cc4dafd8
miketynes <mtynes@uchicago.edu>
2025-11-06 14:43:30 -0600

    remove unused models, update docs

--- cascade/model.py
+++ cascade/model.py
@@ -15,13 +15,15 @@ class AuditStatus(Enum):
 
 @dataclass
 class TrajectorySpec:
+    """Enough information to initialize a trajectory"""
     run_id: hash
-    """Specification for a trajectory"""
-    atoms: Atoms
-    """Initial atoms for the trajectory"""
+    """The run ID"""
     traj_id: int
-    """Which trajectory"""
-    chunk_id: int
+    """The trajectory ID"""
+    target_length: int
+    """The target length of the trajectory"""
+    init_atoms: Atoms
+    """The initial atoms for the trajectory"""
 
 @dataclass
 class AdvanceSpec:
@@ -38,74 +40,3 @@ class AdvanceSpec:
     """Which chunk"""
     steps: int
     """How many steps to run dynamics for"""
-
-
-@dataclass
-class TrajectoryChunk:
-    """
-    Contains the list of atoms generated in a trajectory chunk, along with 
-    relevant metadata
-    """
-
-    atoms: list[Atoms]
-    """List of atoms"""
-
-    model_version: int
-    """Model version that was used to generate this chunk"""
-
-    traj_id: int
-    """Which trajectory this is a part of"""
-
-    chunk_id: int
-    """Which chunk this is"""
-
-    audit_status: AuditStatus = AuditStatus.PENDING
-    """Has this been audited, and if so, has it passed"""
-
-    def __len__(self):
-        return len(self.atoms)
-
-    def __str__(self):
-        return f'Chunk(traj_id={self.traj_id}, chunk_id={self.chunk_id})'
-
-
-@dataclass
-class Trajectory:
-    """A trajectory, composed of TrajectoryChunks"""
-
-    chunks: list[TrajectoryChunk]
-    """The chunks"""
-
-    target_length: int
-    """how long the trajectory should be"""
-
-    id: int
-    """Trajectory ID"""
-
-    init_atoms: Atoms
-    """Initial frame"""
-
-    @property
-    def trajectory(self):
-        out = []
-        for chunk in self.chunks:
-            out.extend(chunk)
-        return out
-
-    def add_chunk(self, chunk: list[Atoms]):
-        self.chunks.append(chunk)
-
-    def __len__(self):
-        return sum(len(chunk) for chunk in self.chunks)
-
-    @property
-    def audit_status(self):
-        return self.chunks[-1].audit_status
-
-    @property
-    def done(self):
-        return (
-            len(self) >= self.target_length
-            and
-            self.audit_status == AuditStatus.PASSED
-        )
