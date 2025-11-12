a8b63fa49bc0a050849d8bdc12434c25274a96c3
miketynes <mtynes@uchicago.edu>
2025-11-12 14:13:17 -0600

    run with more trajectories

--- 3_cascade/run_cascade_academy.sh
+++ 3_cascade/run_cascade_academy.sh
@@ -2,9 +2,12 @@ python run_cascade_academy.py \
     --initial-structures \
         ../0_setup/final-geometries/packmol-CH4-in-H2O=32-seed=1-mace-medium.vasp \
         ../0_setup/final-geometries/packmol-CH4-in-H2O=32-seed=1-mace-medium.vasp \
+        ../0_setup/final-geometries/packmol-CH4-in-H2O=32-seed=1-mace-medium.vasp \
+        ../0_setup/final-geometries/packmol-CH4-in-H2O=32-seed=1-mace-medium.vasp \
     --chunk-size 5 \
     --target-length 25 \
-    --retrain-len 10 \
+    --retrain-len 15 \
+    --retrain-fraction 0.75 \
     --n-sample-frames 5 \
     --accept-rate .5 \
     --learner mace \
