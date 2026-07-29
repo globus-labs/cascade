d36512d8e384670357c98be8588c2ca7f340f229
miketynes <mtynes@uchicago.edu>
2026-07-29 23:58:49 +0000

    launch postgres in userspace

--- scripts/launch_pg.py
+++ scripts/launch_pg.py
@@ -1,8 +1,37 @@
 from subprocess import run, Popen
 from pathlib import Path
-import os
+import os, time
+import psycopg2
 
-db_path = Path('db')
-os.environ['PGDATA'] = str(db_path.absolute())
-run(['pg_ctl', 'initdb'])
-psql_proc = Popen(['postgres', '-D', db_path.absolute()])
+db_path = Path('db').absolute()
+os.environ['PGDATA'] = str(db_path)
+
+if not (db_path / 'PG_VERSION').exists():
+    run(['pg_ctl', 'initdb'])
+
+proc = Popen(['postgres', '-D', str(db_path)])
+
+for _ in range(50):
+    try:
+        conn = psycopg2.connect(dbname='postgres', host='localhost')
+        break
+    except psycopg2.OperationalError:
+        time.sleep(0.3)
+
+conn.autocommit = True
+cur = conn.cursor()
+cur.execute("SELECT 1 FROM pg_roles WHERE rolname='ase'")
+if not cur.fetchone():
+    cur.execute("CREATE ROLE ase WITH LOGIN")
+
+cur.execute("SELECT 1 FROM pg_database WHERE datname='cascade'")
+if not cur.fetchone():
+    cur.execute("CREATE DATABASE cascade OWNER ase")
+
+conn.close()
+
+try:
+    proc.wait()
+except KeyboardInterrupt:
+    proc.terminate()
+    proc.wait()
