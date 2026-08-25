755762de627f99c6eb6c8340c3cf6122f0317df0
miketynes <mtynes@uchicago.edu>
2026-08-25 21:22:08 +0000

    fairchem cascade run

--- 3_cascade/run_cascade_academy.sh
+++ 3_cascade/run_cascade_academy.sh
@@ -1,16 +1,30 @@
+#!/bin/bash
+#SBATCH --partition=main
+#SBATCH --gres=gpu:1
+#SBATCH --exclusive
+
+
+source /home/michael/miniconda3/etc/profile.d/conda.sh
+conda activate cascade
+
 source "${SLURM_SUBMIT_DIR:-$(dirname "${BASH_SOURCE[0]}")}/env_setup.sh"
 python run_cascade_academy.py \
-    --init-config-json init_config_mof_crystalline_200_300K.json \
-    --chunk-size 5 \
-    --target-length 10 \
-    --retrain-len 10 \
-    --retrain-fraction 0.5 \
-    --n-sample-frames 5 \
-    --accept-rate .5 \
+    --max-workers 1 \
+    --init-config-json init_config_mof_crystalline_1000K.json \
+    --chunk-size 1000 \
+    --target-length 5000 \
+    --retrain-len 10000000000 \
+    --retrain-fraction 1 \
+    --n-sample-frames 100 \
+    --n-ensemble 4 \
     --learner mace \
+    --audit-task uq_threshold \
+    --target-ferr 0.8 \
+    --audit-random-fail-rate 0.25 \
+    --calc-type fairchem \
+    --calc-model ../1_ml-potential/uma/uma_omat_ft_mofoff_r2scan.pt \
     --replay-dataset ./datasets/mace-mp/sampled_1000.traj \
     --replay-batch-size 2 \
-    --n-ensemble 4 \
     --log-level DEBUG \
     --device-train cuda:0 \
     --device-label cuda:0 \
